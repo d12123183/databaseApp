70a62d62f80effb341b34ba6ead5c1c59fee1388
Brian <d12123183@mydit.ie>
2016-11-20 12:44:07 +0000

    first commit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,29 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-sqlite.SQLitePlugin",
+        "file": "plugins/cordova-plugin-sqlite/www/SQLitePlugin.js",
+        "pluginId": "cordova-plugin-sqlite",
+        "clobbers": [
+            "window.sqlitePlugin",
+            "cordova.plugins.sqlitePlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.0",
+    "cordova-plugin-device": "1.1.3",
+    "cordova-plugin-sqlite": "1.0.3"
+};
+// BOTTOM OF METADATA
+});
